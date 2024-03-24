a8601bf787b49546e4a55e4ab07aad1ba8361e74
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-03-24 21:27:32 +0300

    Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from "./components/Site.module.css";
 import { PageOne } from "./components/pages/PageOne";
 import { PageTwo } from "./components/pages/PageTwo";
 import { PageThree } from "./components/pages/PageThree";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Error404 } from "./components/pages/Error404";
 
 function App() {
@@ -16,10 +16,12 @@ function App() {
         <div className={styles.nav}>Здесь будет навигация</div>
         <div className={styles.content}>
           <Routes>
+            <Route path="/" element={<Navigate to="/page1" />} />
             <Route path="/page1" element={<PageOne />} />
             <Route path="/page2" element={<PageTwo />} />
             <Route path="/page3" element={<PageThree />} />
-            <Route path="*" element={<Error404 />} />
+            <Route path="/error404" element={<Error404 />} />
+            <Route path="/*" element={<Navigate to="/error404" />} />
           </Routes>
         </div>
       </div>
