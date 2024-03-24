ba3db869ee84af05fa171209a052b725505c90df
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-03-24 23:49:49 +0300

    NavLink with styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import styles from "./components/Site.module.css";
 import { PageOne } from "./components/pages/PageOne";
 import { PageTwo } from "./components/pages/PageTwo";
 import { PageThree } from "./components/pages/PageThree";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 import { Error404 } from "./components/pages/Error404";
 import { NavLink } from "react-router-dom";
+import { error } from "console";
 
 function App() {
   return (
@@ -16,14 +17,38 @@ function App() {
       <div className={styles.body}>
         <div className={styles.nav}>
           <div>
-            <NavLink to={"/page1"}>Page1</NavLink>
+            <NavLink
+              to={"/page1"}
+              className={({ isActive }) =>
+                isActive ? styles.activeNavLink : ""
+              }
+            >
+              Page1
+            </NavLink>
           </div>
           <div>
-            <NavLink to={"/page2"}>Page2</NavLink>
+            <NavLink
+              to={"/page2"}
+              className={({ isActive }) =>
+                isActive ? styles.activeNavLink : ""
+              }
+            >
+              Page2
+            </NavLink>
           </div>
           <div>
-            <NavLink to={"/page3"}>Page3</NavLink>
+            <NavLink
+              to={"/page3"}
+              className={({ isActive }) =>
+                isActive ? styles.activeNavLink : ""
+              }
+            >
+              Page3
+            </NavLink>
           </div>
+          {/* <div>
+            <a href="/page3">page3 HTML</a>
+          </div> */}
         </div>
         <div className={styles.content}>
           <Routes>
