bd283e53f41c351ae8484c875e4a85c14fec952d
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-03-24 21:39:26 +0300

    NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { PageTwo } from "./components/pages/PageTwo";
 import { PageThree } from "./components/pages/PageThree";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Error404 } from "./components/pages/Error404";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
@@ -13,15 +14,25 @@ function App() {
         <h1>HEADER</h1>
       </div>
       <div className={styles.body}>
-        <div className={styles.nav}>Здесь будет навигация</div>
+        <div className={styles.nav}>
+          <div>
+            <NavLink to={"/page1"}>Page1</NavLink>
+          </div>
+          <div>
+            <NavLink to={"/page2"}>Page2</NavLink>
+          </div>
+          <div>
+            <NavLink to={"/page3"}>Page3</NavLink>
+          </div>
+        </div>
         <div className={styles.content}>
           <Routes>
-            <Route path="/" element={<Navigate to="/page1" />} />
+            <Route path="/" element={<Navigate to={"/page1"} />} />
             <Route path="/page1" element={<PageOne />} />
             <Route path="/page2" element={<PageTwo />} />
             <Route path="/page3" element={<PageThree />} />
             <Route path="/error404" element={<Error404 />} />
-            <Route path="/*" element={<Navigate to="/error404" />} />
+            <Route path="/*" element={<Navigate to={"/error404"} />} />
           </Routes>
         </div>
       </div>
